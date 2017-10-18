3eaa6654313edabd6106dd091d66675fcd6d5654
osirisz <earthstroming@hotmail.com>
2017-10-18 14:59:34 +0700

    delete useless page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 import { ActivePage } from './../pages/active/active';
 import { CompletePage } from './../pages/complete/complete';
 import { CancelPage } from './../pages/cancel/cancel';
